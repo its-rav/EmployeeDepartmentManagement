d0e0b1491ceb48b4e0e37916fed8c30df6f4060c
Nhân Võ <NhanVTSE130478@fpt.edu.vn>
2020-10-01 23:08:56 +0700

    DbContext configuration

--- EmployeeDepartmentManagement/Startup.cs
+++ EmployeeDepartmentManagement/Startup.cs
@@ -1,7 +1,9 @@
 using Autofac;
+using DataTier.Models;
 using DataTier.UnitOfWork;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,6 +21,8 @@ namespace EmployeeDepartmentManagement
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //sql connection
+            services.AddDbContext<EmployeeDepartmentManagementContext>(options =>options.UseSqlServer(Configuration.GetConnectionString("db")));
             services.AddControllers();
         }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

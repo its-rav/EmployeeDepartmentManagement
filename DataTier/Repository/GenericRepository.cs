f3a16cc821d2392a59c22ad34f545c6cf92b5699
Nhân Võ <NhanVTSE130478@fpt.edu.vn>
2020-10-01 22:27:23 +0700

    Add GenericRepository and UnitOfWork

---
+++ DataTier/Repository/GenericRepository.cs
@@ -0,0 +1,65 @@
+﻿
+using DataTier.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTier.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected FIEPContext _context;
+        protected DbSet<T> table = null;
+
+        public GenericRepository(FIEPContext context)
+        {
+            this._context = context;
+            table = _context.Set<T>();
+        }
+
+        public int Commit()
+        {
+            return _context.SaveChanges();
+        }
+
+        public IQueryable<T> FindAllByProperty(Func<T, bool> expression)
+        {
+            return table.Where(expression).AsQueryable();
+        }
+
+        public T FindFirstByProperty(Func<T, bool> expression)
+        {
+            return table.FirstOrDefault(expression);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return table.Select(x => x);
+        }
+        public EntityEntry<T> Insert(T obj)
+        {
+            var result = table.Add(obj);
+            return result;
+        }
+
+        public void InsertRange(List<T> obj)
+        {
+            table.AddRange(obj);
+        }
+
+        public void Update(T obj)
+        {
+            table.Update(obj);
+        }
+
+        public void UpdateRange(List<T> obj)
+        {
+            table.UpdateRange(obj);
+        }
+
+
+    }
+}
---
+++ DataTier/Repository/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTier.Repository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        EntityEntry<T> Insert(T obj);
+        void Update(T obj);
+        void InsertRange(List<T> obj);
+        void UpdateRange(List<T> obj);
+        IQueryable<T> FindAllByProperty(Func<T, bool> expression);
+        T FindFirstByProperty(Func<T, bool> expression);
+        int Commit();
+    }
+}
